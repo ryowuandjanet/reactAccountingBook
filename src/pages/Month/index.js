82096d801d451d93addf3a7afdc611fe891d3cc4
ryowuandjanet <ryowu0329@gmail.com>
2023-12-23 13:33:46 +0800

    Day5-01-月度帳單-功能演示和結構搭建

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import { NavBar, DatePicker } from "antd-mobile"
+import './index.scss'
 
 const Month = () => {
   return (
-    <div>Month</div>
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          <div className="date">
+            <span className="text">
+              2023 | 3月帳單
+            </span>
+            <span className="arrow expand"></span>
+          </div>
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">結餘</span>
+            </div>
+          </div>
+          <DatePicker
+            className="kaDate"
+            title="記帳日期"
+            precision="month"
+            visible={false}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
